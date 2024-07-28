604e9df42dd72593ae528248e587df9062bf59d9
Gabriel <souzagabriel0006@gmail.com>
2024-07-27 23:10:57 -0300

    upload do projeto

--- galeria/views.py
+++ galeria/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
     # quando chegar uma requisição para o index, responda de acordo com o return
-    return HttpResponse(
-        '<h1>Hello World</h1>'
-        '<p>Agora funcionou</p>'
-    )
+    return render(request, 'galeria/index.html')
+
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
